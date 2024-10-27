f44ec017c7f0267b33646c51d16e2b0eadf8c527
Boris Todorov <btodorovtodorov@gmail.com>
2024-10-27 11:24:48 +0200

    task 2 added

--- 01_assynchronous_programming/Lab/02.Github-Repos/scripts/app.js
+++ 01_assynchronous_programming/Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,24 @@
 function loadRepos() {
-	console.log("TODO...");
+    const username = document.querySelector('#username').value;
+    const url = `https://api.github.com/users/${username}/repos`;
+    const reposUl = document.querySelector('#repos');
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((repos) => {
+            reposUl.innerHTML = '';
+
+            repos.forEach((repo) => {
+                // console.log(repo);
+
+                const aElm = document.createElement('a');
+                aElm.href = repo.html_url;
+                aElm.textContent = repo.full_name;
+
+                const liElm = document.createElement('li');
+                liElm.appendChild(aElm);
+
+                reposUl.appendChild(liElm);
+            });
+        });
 }
